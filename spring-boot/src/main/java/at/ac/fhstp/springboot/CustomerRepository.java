56f4465f0258c37792f82adc36292e04f26a7c17
Martin Vasko <martin@expressflow.com>
2022-05-10 07:52:40 +0200

    Refactored Spring Boot sample

---
+++ spring-boot/src/main/java/at/ac/fhstp/springboot/CustomerRepository.java
@@ -0,0 +1,12 @@
+package at.ac.fhstp.springboot;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Customer findById(long id);
+}
